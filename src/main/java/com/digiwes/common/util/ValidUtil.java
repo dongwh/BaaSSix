c6a2aeb2e3593d49d4b02825fa0542c5046facc9
Zhao YunPing <zhaoyp@asiainfo.com>
2015-07-13 15:37:39 +0800

    修改包名

---
+++ src/main/java/com/digiwes/common/util/ValidUtil.java
@@ -0,0 +1,16 @@
+package com.digiwes.common.util;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * Created by zhaoyp on 2015/7/13.
+ */
+public class ValidUtil {
+
+    public static boolean checkObjectIsNull(Object param){
+          if( null == param ){
+              return true;
+          }
+        return false;
+    }
+}
